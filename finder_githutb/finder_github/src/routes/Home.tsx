b194d10b62e90f8fad09f66b4fbbd3e088b54789
Ayanara <aya.nathane@gmail.com>
2023-10-25 16:22:05 -0300

    feat: cria o type

--- finder_githutb/finder_github/src/routes/Home.tsx
+++ finder_githutb/finder_github/src/routes/Home.tsx
@@ -1,6 +1,11 @@
 import Search from "../componentes/Search";
 
+import { useState } from "react";
+// Necessário para fazer a requisição do usuário
+
 const Home = () => {
+  const [ user, setUser ] = useState(null);
+  // Necessário para salvar o estado do usuário, esse estado começa como null
   return (
    <Search />
   )

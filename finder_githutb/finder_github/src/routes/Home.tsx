795a42bd117b8cba3cab77ead2e5b1a8a691a250
Ayanara <aya.nathane@gmail.com>
2023-10-26 18:35:38 -0300

    feat: implemtenta a api e testa se os dados estão sendo chamados corretamente

--- finder_githutb/finder_github/src/routes/Home.tsx
+++ finder_githutb/finder_github/src/routes/Home.tsx
@@ -5,18 +5,34 @@ import { useState } from "react";
 // Necessário para fazer a requisição do usuário
 
 const Home = () => {
-  const [ user, setUser ] = useState<UserProps | null>(null);
+  const [user, setUser] = useState<UserProps | null>(null);
   // Necessário para salvar o estado do usuário, esse estado começa como null
 
   const loadUser = async (userName: string) => {
     const res = await fetch(`http://api.github.com/users/${userName}`)
     const data = await res.json()
 
-    console.log(data)
-  }
+    const { avatar_url, login, location, followers, following } = data
+
+    const userDate: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following
+    };
+    setUser(userDate);
+
+  };
+
   return (
-   <Search loadUser={loadUser}/>
+    <div>
+      <Search loadUser={loadUser} />
+      {user && <p>{user?.login}</p>}
+      {user && <p>{user?.followers}</p>}
+      {user && <p>{user?.following}</p>}
+      {user && <p>{user?.location}</p>}
+    </div>
   )
 }
-
-export default Home
+export default Home;

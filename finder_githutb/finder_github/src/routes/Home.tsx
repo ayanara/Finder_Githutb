cc7c1de857842dd2eb1be225c7e566b06d923eef
Ayanara <aya.nathane@gmail.com>
2023-10-26 18:51:11 -0300

    implementa as busca dos dados

--- finder_githutb/finder_github/src/routes/Home.tsx
+++ finder_githutb/finder_github/src/routes/Home.tsx
@@ -12,14 +12,16 @@ const Home = () => {
     const res = await fetch(`http://api.github.com/users/${userName}`)
     const data = await res.json()
 
-    const { avatar_url, login, location, followers, following } = data
+    const { avatar_url, login, location, followers, following, name, bio } = data
 
     const userDate: UserProps = {
       avatar_url,
       login,
       location,
       followers,
-      following
+      following,
+      name,
+      bio,
     };
     setUser(userDate);
 
@@ -28,7 +30,10 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <img src={user?.avatar_url} alt={user.name}/>}
       {user && <p>{user?.login}</p>}
+      {user && <p>{user?.name}</p>}
+      {user && <p>{user?.bio}</p>}
       {user && <p>{user?.followers}</p>}
       {user && <p>{user?.following}</p>}
       {user && <p>{user?.location}</p>}
--- finder_githutb/finder_github/src/types/user.ts
+++ finder_githutb/finder_github/src/types/user.ts
@@ -1,5 +1,7 @@
 export type UserProps = {
   avatar_url: string;
+  name: string;
+  bio: string;
   login: string;
   location: string;
   followers: number;
